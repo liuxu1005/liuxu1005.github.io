34b235492695ef3a312759cd83e557543ff9add4
Xu <liuxu1005@gmail.com>
2016-06-22 10:55:50 -0400

    remove error handler

--- graphics/main.js
+++ graphics/main.js
@@ -1,24 +1,4 @@
 
-function text2html(text) {
-  return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '<br>');
-}
-
-function handleError(text) {
-  var html = text2html(text);
-  if (html == 'WebGL not supported') {
-    html = 'Your browser does not support WebGL.<br>Please see\
-    <a href="http://www.khronos.org/webgl/wiki/Getting_a_WebGL_Implementation">\
-    Getting a WebGL Implementation</a>.';
-  }
-  var loading = document.getElementById('loading');
-  loading.innerHTML = html;
-  loading.style.zIndex = 1;
-}
-
-window.onerror = handleError;
-
-
-
 
 var canvas;
 var gl;
